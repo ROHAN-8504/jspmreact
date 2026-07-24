55a9273c9398b98db0a327c7a8fb2ff8858bdd46
ROHAN-8504 <203j1a4207@raghuinstech.com>
2026-07-24 14:40:57 +0530

    styling

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,17 @@
 import Header from './Header'
 import Home from './Home'
 import './App.css'
+import Card from './Card'
 function App() {
 
   return (
     <>
     <Header/>
     <Home/>
+     <Card/>
+     <Card/>
+     <Card/>
+     
     </>
   )
 }
--- src/Card.jsx
+++ src/Card.jsx
@@ -2,8 +2,8 @@
 function Card() {
   return (
     <>
-    <div>
-        <img src="https://dineout-media-assets.swiggy.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_600,h_400/DINEOUT_ALL_RESTAURANTS/IMAGES/RESTAURANT_IMAGE_SERVICE/2025/11/5/c48b6233-75f2-4577-98d9-4fb0dd555f30_image1968203e8e28a4bd9a3355443044bdcd1.JPG" alt="" />
+    <div style={{border:"1px solid black",backgroundColor:"lightseagreen"}}   >
+        <img height={250} width={250}   src="https://dineout-media-assets.swiggy.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_600,h_400/DINEOUT_ALL_RESTAURANTS/IMAGES/RESTAURANT_IMAGE_SERVICE/2025/11/5/c48b6233-75f2-4577-98d9-4fb0dd555f30_image1968203e8e28a4bd9a3355443044bdcd1.JPG" alt="" />
         <p>Petuk Maharaaj</p>
         <p>Infinia Tower, Chandrasekharpur, Bhubaneswar</p>
     </div>
