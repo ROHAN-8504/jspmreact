c17a3fc83b912f083ab67a7cc9d1708d781debb6
ROHAN-8504 <203j1a4207@raghuinstech.com>
2026-07-24 14:19:51 +0530

    styling

--- src/Header.jsx
+++ src/Header.jsx
@@ -2,7 +2,9 @@ import Nav from "./Nav"
 function Header() {
   return (
     <>
-    <header>
+    <header style={{display:"flex",
+        justifyContent:"space-evenly"
+        }} >
     <img src="src/assets/vite.svg" alt="" />
      <Nav/>
     </header>
--- src/Nav.jsx
+++ src/Nav.jsx
@@ -3,7 +3,7 @@
 function Nav() {
   return (
     <>
-    <nav>
+    <nav style={{display:"flex",gap:"20px"}} >
     <a href="">Signup</a>
     <a href="">About</a>
     <a href="">Products</a>
