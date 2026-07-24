885fc97c19afccae2c46963213514ddeeafb8762
ROHAN-8504 <203j1a4207@raghuinstech.com>
2026-07-24 13:55:15 +0530

    first commit

---
+++ src/Header.jsx
@@ -0,0 +1,13 @@
+import Nav from "./Nav"
+function Header() {
+  return (
+    <>
+    <header>
+    <img src="src/assets/vite.svg" alt="" />
+     <Nav/>
+    </header>
+    </>
+  )
+}
+
+export default Header
---
+++ src/Nav.jsx
@@ -0,0 +1,17 @@
+
+
+function Nav() {
+  return (
+    <>
+    <nav>
+    <a href="">Signup</a>
+    <a href="">About</a>
+    <a href="">Products</a>
+    <a href="">Pricing</a>
+    <a href="">Support</a>
+    </nav>
+    </>
+  )
+}
+
+export default Nav
