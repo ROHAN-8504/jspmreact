f6e005422d3e2558b061b2783725ceb9270482f0
ROHAN-8504 <203j1a4207@raghuinstech.com>
2026-07-25 11:28:38 +0530

    usestate exaample

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,26 @@
 import Header from './Header'
 import Home from './Home'
+import { useState } from 'react'
 import './App.css'
 import Card from './Card'
 import restaurants from '../restraunts'
 function App() {
+
+
+const [Count, setCount] = useState(0)
+
+ 
+  let handlecount=()=>{
+    setCount(Count+1)
+  }
+
   return (
     <>
     <Header/>
-    <Home/>
+    <Home/>    
      <Card {...restaurants} />
      
+    <button onClick={handlecount}>Count {Count}</button>
     </>
   )
 }
